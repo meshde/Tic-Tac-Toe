45170795a05512bf119954bb04be1d8aa346ba53
meshde <meshde.md@gmail.com>
2017-06-05 10:53:15 +0530

    Added Player class and subclasses

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
 import os
+import random
 class State(object):
 	"""Each State Represents a state of the game(a particular configuration of X's and O's in the game)"""
 	def __init__(self,player,mat=None,rem=9):
@@ -20,6 +21,8 @@ class State(object):
 		self.remaining = rem
 		# The winner attribute specifies the winner in the current state, if any
 		self.winner = 0
+		# Q matrix for Q Learning
+		self.Q = []
 	def get_other_player(self):
 		if self.player == 'X':
 			return 'O'
@@ -39,6 +42,8 @@ class State(object):
 					child.generate()
 					# Add the child node to the children list
 					self.children.append(child)
+					# Initialise Q matrix value for the child
+					self.Q.append(0)
 	def is_over(self):
 		if self.get_winner():
 			return True
@@ -128,6 +133,10 @@ class State(object):
 		return self.player
 	def get_chosen(self):
 		return self.chosen
+	def get_children(self):
+		return self.children
+	def get_max_Q(self):
+		return max(self.Q)
 	def print_state(self):
 		os.system('cls')
 		print('\t \t|\t \t|\t \t')
@@ -153,27 +162,102 @@ class Game(object):
 		#print(self.state.get_chosen())
 
 		# The map attribute is used for user input. The user is asked to enter a number from 1 to 9 where 1 represents cell (0,0), 2 represents cell (0,1), 3 represents cell (0,2) and so on
-		self.map = [None,(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
-		self.user = user
+		# self.map = [None,(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+		# self.user = user
+		# self.history = []
 	def get_state_from_user(self):
 		u = int(input('Enter the index of the block:\t'))
 		mat = [[w for w in z] for z in self.state.get_matrix()]
 		x,y = self.map[u]
 		mat[x][y] = self.user
 		return self.state.get_child(mat)
-	def play(self):
+	def play(self,player1,player2):
+		# Assuming Player 1 is X (for now)
 		while not self.state.is_over():
 			self.state.print_state()
 			if self.state.get_player() == self.user:
-				self.state = self.get_state_from_user()
+				self.state = self.get_move(player1)
 			else:
-				print('CPU made a move')
-				self.state = self.state.get_chosen()
+				#print('CPU made a move')
+				self.state = self.get_move(player2)
 		self.state.print_state()
 		win = self.state.get_winner()
 		if not win:
 			print('DRAW')
-		elif self.user == win:
+		elif 'X' == win:
+			print(player1+' Won!')
+		else:
+			print(player2+' Won!')
+	def get_move(self,player):
+		if player == 'User':
+			return self.get_state_from_user()
+		if player == 'MiniMax':
+			print('MiniMax made a move!')
+			return self.state.get_chosen()
+		if player == 'Q':
+			return self.Q_move()
+	def Q_move(self):
+
+class Player(object):
+	def __init__(self,player):
+		""" Player attribute represents whether player is X or O """
+		self.player = player
+	def make_move(self,state):
+		return
+	def post_result(self,res):
+		return
+
+class User(Player):
+	def __init__(self,player):
+		super().__init__(player)
+		self.map = [None,(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
+	def make_move(self,state):
+		u = int(input('Enter the index of the block:\t'))
+		mat = [[w for w in z] for z in state.get_matrix()]
+		x,y = self.map[u]
+		mat[x][y] = self.player
+		return state.get_child(mat)
+	def post_result(self,res):
+		if not res:
+			print('DRAW!')
+		elif res == self.player:
 			print('You Won!')
 		else:
 			print('You Lost!')
+		return
+
+class QPlayer(Player):
+	def __init__(self,player):
+		super().__init__(player)
+		self.history = []
+		self.epsilon = 0.3
+	def clear(self):
+		self.history.clear()
+		return
+	def make_move(self,state):
+		if random.random() < self.epsilon:
+			""" Exploration """
+			index = random.randint(0,len(state.get_children())-1)
+		else:
+			""" Exploitation """
+			maxQ = state.get_max_Q()
+			best = [i for i,a in enumerate(state.get_children()) if a == maxQ]
+			index = random.choice(best)
+		self.history.append((state,index))
+		return state.get_children()[index]
+	def post_result()
+
+class MiniMax(Player):
+	def __init__(self,player):
+		super().__init__(player)
+	def make_move(self,state):
+		print('MiniMax made a Move')
+		return state.get_chosen()
+	def post_result(self,res):
+		if not res:
+			print('DRAW!')
+		elif res == self.player:
+			print('MiniMax Won!')
+		else:
+			print('MiniMax Lost!')
+		return
